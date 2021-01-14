931bb0ede60a7643f7bb78db5309f25dad8b5995
Colin Murphy <cpmurphy@ucsc.edu>
2021-01-13 21:52:25 -0800

    Add files via upload

--- flask/app.py
+++ flask/app.py
@@ -1,15 +1,15 @@
 from flask import Flask, request, render_template, redirect, url_for
 import os
-from PIL import Image
-import io
-import torchvision.transforms as transforms
+# from PIL import Image
+# import io
+# import torchvision.transforms as transforms
 
-photos_folder = os.path.join('static', 'photos')
+# photos_folder = os.path.join('static', 'photos')
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = photos_folder
+# app.config['UPLOAD_FOLDER'] = photos_folder
 
-from inference import classify, segment
+# from inference import classify, segment
 
 
 @app.after_request
@@ -33,44 +33,44 @@ def hello_world():
         return render_template('index.html', value='hi')
 
     # send image
-    if request.method == 'POST':
-        print(request.files)
-        if 'file' not in request.files:
-            print('file not uploaded')
-            return redirect(url_for('hello_world'))
-        file = request.files['file']
+    # if request.method == 'POST':
+    #     print(request.files)
+    #     if 'file' not in request.files:
+    #         print('file not uploaded')
+    #         return redirect(url_for('hello_world'))
+    #     file = request.files['file']
 
-        # Save, resize, and resave image
-        file.save("static/photos/original.jpg")
-        image = Image.open('static/photos/original.jpg')
-        my_transforms = transforms.Compose([
-        transforms.Resize(256)])
-        image = my_transforms(image)
-        image.save("static/photos/original.jpg")
+    #     # Save, resize, and resave image
+    #     file.save("static/photos/original.jpg")
+    #     image = Image.open('static/photos/original.jpg')
+    #     my_transforms = transforms.Compose([
+    #     transforms.Resize(256)])
+    #     image = my_transforms(image)
+    #     image.save("static/photos/original.jpg")
 
-        # Run image through classification function
-        resultClass = classify(image)
-        # Run image through Segmentation function
-        resultSeg = segment(image)
+    #     # Run image through classification function
+    #     resultClass = classify(image)
+    #     # Run image through Segmentation function
+    #     resultSeg = segment(image)
 
-        # Save Segmented image
-        full_filename = os.path.join("/", app.config['UPLOAD_FOLDER'], 'file.jpg')
-        resultSeg.convert('RGB').save("static/photos/segmention.jpg")
+    #     # Save Segmented image
+    #     full_filename = os.path.join("/", app.config['UPLOAD_FOLDER'], 'file.jpg')
+    #     resultSeg.convert('RGB').save("static/photos/segmention.jpg")
 
-        # Redirect to result page with required information
-        return redirect(url_for('getResult', resultClass=resultClass, resultSeg='segmention.jpg', original='original.jpg'))
+    #     # Redirect to result page with required information
+    #     return redirect(url_for('getResult', resultClass=resultClass, resultSeg='segmention.jpg', original='original.jpg'))
 
-@app.route('/result/<resultClass>/<resultSeg>/<original>', methods=['GET', 'POST'])
-def getResult(resultClass='beans', resultSeg='donnie.jpg', original='original.jpg'):
-    # get page
-    if request.method == 'GET':
-        segFile = os.path.join('/', app.config['UPLOAD_FOLDER'], resultSeg)
-        originalFile = os.path.join('/', app.config['UPLOAD_FOLDER'], original)
-        return render_template('result.html', flower=resultClass, seg=segFile, original=originalFile)
+# @app.route('/result/<resultClass>/<resultSeg>/<original>', methods=['GET', 'POST'])
+# def getResult(resultClass='beans', resultSeg='donnie.jpg', original='original.jpg'):
+#     # get page
+#     if request.method == 'GET':
+#         segFile = os.path.join('/', app.config['UPLOAD_FOLDER'], resultSeg)
+#         originalFile = os.path.join('/', app.config['UPLOAD_FOLDER'], original)
+#         return render_template('result.html', flower=resultClass, seg=segFile, original=originalFile)
 
-    # Redirect back to Homepage
-    if request.method == 'POST':
-        return redirect(url_for('hello_world'))
+#     # Redirect back to Homepage
+#     if request.method == 'POST':
+#         return redirect(url_for('hello_world'))
 
 
 if __name__ == '__main__':
